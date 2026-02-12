92c758d8eee6f1bf7ccd98e4330e9b2212fcc637
Leonardo474-cpu <leo8minaya@gmail.com>
2026-02-11 22:57:01 -0500

    Metodo get transferencias

--- apps/logistics/src/core/warehouse/transfer/application/service/transfer-command.service.ts
+++ apps/logistics/src/core/warehouse/transfer/application/service/transfer-command.service.ts
@@ -236,6 +236,10 @@ export class TransferCommandService implements TransferPortsIn {
     return transfer;
   }
 
+  getAllTransfers(): Promise<Transfer[]> {
+    return this.transferRepo.findAll();
+  }
+
   // --- Validaciones Auxiliares ---
 
   private async validateWarehouseBelongsToHeadquarters(
